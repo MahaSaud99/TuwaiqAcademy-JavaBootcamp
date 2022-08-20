201668d55bc055423a21caeb9d9e864cdff764dd
Maha Alyemeni <52765342+MahaSaud99@users.noreply.github.com>
2022-08-20 21:42:01 +0300

    Add files via upload

---
+++ FinalProject-BackEnd/Maawa/src/main/java/com/example/maawa/MaawaApplication.java
@@ -0,0 +1,13 @@
+package com.example.maawa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaawaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaawaApplication.class, args);
+    }
+
+}
---
+++ FinalProject-BackEnd/Maawa/src/test/java/com/example/maawa/MaawaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.maawa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaawaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
